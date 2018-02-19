f31d0e77b26048eb1a39c4c4a8d618dd3d232c70
GK <gisela0108@github.com>
2018-02-19 23:54:28 +0700

    income outcome dao edit

--- dao/IncomeDaoImpl.php
+++ dao/IncomeDaoImpl.php
@@ -30,4 +30,37 @@ class IncomeDaoImpl {
         }
         PDOUtil::closePDOConnection($link);
     }
+    
+    public function updateIncome (Income $income) {
+        $link = PDOUtil::createPDOConnection();
+        $query = "UPDATE Income SET amount = ? , description = ?, date = ?, IncomeCategory_id = ?, User_id = ? WHERE id = ?";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1, $income->getAmount(), PDO::PARAM_STR);
+        $stmt->bindValue(2, $income->getDescription(), PDO::PARAM_STR);
+        $stmt->bindValue(3, $income->getDate(), PDO::PARAM_STR);
+        $stmt->bindValue(4, $income->getCategory()->getId(), PDO::PARAM_STR);
+        $stmt->bindValue(5, $income->getUser()->getId(), PDO::PARAM_STR);
+        $stmt->bindValue(6, $income->getId(), PDO::PARAM_STR);
+        $link->beginTransaction();
+        if ($stmt->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        PDOUtil::closePDOConnection($link);
+    }
+
+    public function deleteIncome(Income $income) {
+        $link = PDOUtil::createPDOConnection();
+        $query = "DELETE FROM Income WHERE id = ?";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1, $income->getId(), PDO::PARAM_INT);
+        $link->beginTransaction();
+        if ($stmt->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        PDOUtil::closePDOConnection($link);
+    }
 }
--- dao/OutcomeDaoImpl.php
+++ dao/OutcomeDaoImpl.php
@@ -30,4 +30,37 @@ class IncomeDaoImpl {
         }
         PDOUtil::closePDOConnection($link);
     }
+    
+    public function updateOutcome (Outcome $outcome) {
+        $link = PDOUtil::createPDOConnection();
+        $query = "UPDATE Outcome SET amount = ? , description = ?, date = ?, IncomeCategory_id = ?, User_id = ? WHERE id = ?";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1, $outcome->getAmount(), PDO::PARAM_STR);
+        $stmt->bindValue(2, $outcome->getDescription(), PDO::PARAM_STR);
+        $stmt->bindValue(3, $outcome->getDate(), PDO::PARAM_STR);
+        $stmt->bindValue(4, $outcome->getCategory()->getId(), PDO::PARAM_STR);
+        $stmt->bindValue(5, $outcome->getUser()->getId(), PDO::PARAM_STR);
+        $stmt->bindValue(6, $outcome->getId(), PDO::PARAM_STR);
+        $link->beginTransaction();
+        if ($stmt->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        PDOUtil::closePDOConnection($link);
+    }
+
+    public function deleteOutcome(Outcome $outcome) {
+        $link = PDOUtil::createPDOConnection();
+        $query = "DELETE FROM Outcome WHERE id = ?";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1, $outcome->getId(), PDO::PARAM_INT);
+        $link->beginTransaction();
+        if ($stmt->execute()) {
+            $link->commit();
+        } else {
+            $link->rollBack();
+        }
+        PDOUtil::closePDOConnection($link);
+    }
 }
